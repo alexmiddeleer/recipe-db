37fa55ab5d1fc2287f37d013c426bd7445788c56
Alex Middeleer <almiddeleer@gmail.com>
2014-06-29 15:32:43 -0400

    created rough draft of database

---
+++ recipes.sql
@@ -0,0 +1,77 @@
+CREATE TABLE IF NOT EXISTS recipe(
+   recipeID INTEGER PRIMARY KEY, 
+   userID INTEGER,
+   categories TEXT,
+   title TEXT,
+   source TEXT,
+   author TEXT,
+   serves INTEGER,
+   cookTimeHours INTEGER,
+   cookTimeMinutes INTEGER,
+   prepTimeHours INTEGER,
+   prepTimeMinutes INTEGER,
+   date TEXT,
+   nutrNotes TEXT
+);
+
+CREATE TABLE IF NOT EXISTS food(
+   foodID INTEGER PRIMARY KEY,
+   userID INTEGER,
+   categories TEXT,
+   name TEXT,
+   nutrNotes TEXT
+);
+
+CREATE TABLE IF NOT EXISTS ingredient(
+   ingredientID INTEGER PRIMARY KEY,
+   substituteIngrID INTEGER,
+   userID INTEGER,
+   foodID INTEGER,
+   recipeID INTEGER
+   categories TEXT,
+   name TEXT,
+   quantity INTEGER,
+   weight REAL,
+   weightUnit TEXT,
+   volume REAL,
+   volumeUnit TEXT,
+   price REAL,
+);
+
+CREATE TABLE IF NOT EXISTS ingrInstr(
+   ingrInstrID INTEGER PRIMARY KEY,
+   ingredientID INTEGER,
+   instructionID INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS instruction(
+   instructionID INTEGER PRIMARY KEY,
+   recipeID INTEGER
+   userID INTEGER,
+   categories TEXT,
+   stepNum INTEGER,
+   isOptional INTEGER,
+   notes TEXT,
+   startTimeHrs INTEGER,
+   startTimeMin INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS favoriteRecipes(
+   favRecID INTEGER,
+   userID INTEGER,
+   recipeID INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS user(
+   userID INTEGER PRIMARY KEY,
+   name TEXT,
+   password TEXT,
+   username TEXT,
+);  
+
+CREATE TABLE IF NOT EXISTS shoppingList(
+   shoppingListID INTEGER PRIMARY KEY,
+   userID INTEGER,
+   date TEXT,
+   cost REAL
+); 
