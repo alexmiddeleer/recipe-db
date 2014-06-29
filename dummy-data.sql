9b2c3b51170236b182243530cb0edbe1e1549756
Alex Middeleer <almiddeleer@gmail.com>
2014-06-29 17:41:35 -0400

    finished adding dummy data and finalizing first draft of recipes db schema

--- dummy-data.sql
+++ dummy-data.sql
@@ -1,4 +1,4 @@
-INSERT INTO recipe(recipeID, userID, categories, title, source)
+INSERT INTO recipe(recipeID, userID, categories, name, source)
    values
      (1, 1,'fish,dinner','Nice Cod', 'alexmiddeleer.com')
    , (2, 1,'beef,dinner','Good Steak', 'google.com')
@@ -14,8 +14,8 @@ INSERT INTO food(foodID, userID, categories, name)
 
 INSERT INTO ingredient(ingredientID, userID, foodID, recipeID, weight, weightUnit)
    values
-     (1, 1, 1, '1', 'pound')
-   , (2, 1, 2, '2', 'pound')
+     (1, 1, 1, 1, '1', 'pound')
+   , (2, 1, 2, 2, '2', 'pound')
 ;
 
 INSERT INTO ingrInstr(ingrInstrID, ingredientID, instructionID)
@@ -27,11 +27,26 @@ INSERT INTO ingrInstr(ingrInstrID, ingredientID, instructionID)
 INSERT INTO instruction(instructionID, userID, recipeID, text, stepNum)
    values
      (1, 1, 1, "cut up fish", 1)
-     (2, 1, 1, "add salt", 2)
+   , (2, 1, 1, "add salt", 2)
    , (3, 1, 1, "cook in pan", 3)
    , (4, 1, 2, "trim beef", 1)
    , (5, 1, 2, "add salt or tenderizer", 2)
    , (6, 1, 2, "grill to perfection", 3)
 ;
 
+INSERT INTO favoriteRecipes(favRecID, userID, recipeID)
+   values
+     (1, 1, 1)
+;
 
+INSERT INTO user(userID, firstName, lastName, password, email)
+   values
+     (1, 'Alex', 'Middeleer', 'foobar', 'almiddeleer@gmail.com')
+   , (2, 'John', 'Doe', 'barfoo', 'johndoe@nowhere.com')
+;
+
+INSERT INTO shoppingList(shoppingListID, userID, date, cost)
+   values
+     (1, 1, '1/1/2011', 123.45)
+   , (2, 2, '2/2/2022', 65.43)
+;
--- recipes.sql
+++ recipes.sql
@@ -1,77 +1,78 @@
 CREATE TABLE IF NOT EXISTS recipe(
-   recipeID INTEGER PRIMARY KEY, 
-   userID INTEGER,
-   categories TEXT,
-   name TEXT,
-   source TEXT,
-   author TEXT,
-   serves INTEGER,
-   cookTimeHours INTEGER,
-   cookTimeMinutes INTEGER,
-   prepTimeHours INTEGER,
-   prepTimeMinutes INTEGER,
-   date TEXT,
-   nutrNotes TEXT
+   recipeID INTEGER PRIMARY KEY 
+   , userID INTEGER
+   , categories TEXT
+   , name TEXT
+   , source TEXT
+   , author TEXT
+   , serves INTEGER
+   , cookTimeHours INTEGER
+   , cookTimeMinutes INTEGER
+   , prepTimeHours INTEGER
+   , prepTimeMinutes INTEGER
+   , date TEXT
+   , nutrNotes TEXT
 );
 
 CREATE TABLE IF NOT EXISTS food(
-   foodID INTEGER PRIMARY KEY,
-   userID INTEGER,
-   categories TEXT,
-   name TEXT,
-   nutrNotes TEXT
+   foodID INTEGER PRIMARY KEY
+   , userID INTEGER
+   , categories TEXT
+   , name TEXT
+   , nutrNotes TEXT
 );
 
 CREATE TABLE IF NOT EXISTS ingredient(
-   ingredientID INTEGER PRIMARY KEY,
-   substituteIngrID INTEGER,
-   userID INTEGER,
-   foodID INTEGER,
-   recipeID INTEGER
-   categories TEXT,
-   quantity INTEGER,
-   weight REAL,
-   weightUnit TEXT,
-   volume REAL,
-   volumeUnit TEXT,
-   price REAL
+   ingredientID INTEGER PRIMARY KEY
+   , substituteIngrID INTEGER
+   , userID INTEGER
+   , foodID INTEGER
+   , recipeID INTEGER
+   , categories TEXT
+   , quantity INTEGER
+   , weight REAL
+   , weightUnit TEXT
+   , volume REAL
+   , volumeUnit TEXT
+   , price REAL
 );
 
 CREATE TABLE IF NOT EXISTS ingrInstr(
-   ingrInstrID INTEGER PRIMARY KEY,
-   ingredientID INTEGER,
-   instructionID INTEGER
+   ingrInstrID INTEGER PRIMARY KEY
+   , ingredientID INTEGER
+   , instructionID INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS instruction(
-   instructionID INTEGER PRIMARY KEY,
-   recipeID INTEGER
-   userID INTEGER,
-   text TEXT,
-   categories TEXT,
-   stepNum INTEGER,
-   isOptional INTEGER,
-   notes TEXT,
-   startTimeHrs INTEGER,
-   startTimeMin INTEGER
+   instructionID INTEGER PRIMARY KEY
+   , recipeID INTEGER
+   , userID INTEGER
+   , text TEXT
+   , categories TEXT
+   , stepNum INTEGER
+   , isOptional INTEGER
+   , notes TEXT
+   , startTimeHrs INTEGER
+   , startTimeMin INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS favoriteRecipes(
-   favRecID INTEGER,
-   userID INTEGER,
-   recipeID INTEGER
+   favRecID INTEGER
+   , userID INTEGER
+   , recipeID INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS user(
-   userID INTEGER PRIMARY KEY,
-   name TEXT,
-   password TEXT,
-   username TEXT
+   userID INTEGER PRIMARY KEY
+   , firstName TEXT
+   , lastName TEXT
+   , password TEXT
+   , email TEXT
 );  
 
 CREATE TABLE IF NOT EXISTS shoppingList(
-   shoppingListID INTEGER PRIMARY KEY,
-   userID INTEGER,
-   date TEXT,
-   cost REAL
+   shoppingListID INTEGER PRIMARY KEY
+   , userID INTEGER
+   , date TEXT
+   , cost REAL
 ); 
