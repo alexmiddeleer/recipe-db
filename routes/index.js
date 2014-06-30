5d0035d6b8ac3664fab1a6573bbbf3349b5beab7
Alex Middeleer <almiddeleer@gmail.com>
2014-06-29 22:11:15 -0400

    added sqlite and wrote a simple 'hello recipes' program

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
+var express = require('express')
+  , sqlite3 = require('sqlite3').verbose()
+  , ready = false;
+;
+
 var router = express.Router();
+
+db = new sqlite3.Database('recipes.db', sqlite3.OPEN_READONLY, function(err) {
+   if(!err){
+     ready=true;
+   }
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  if (ready) {
+   var recipes = [];
+   db.serialize(function() {
+      db.each("SELECT name AS name FROM recipe;", function(err, row){
+         if (err) {
+            res.err("err:"+err);
+         }
+         recipes.push(row.name);
+      }, function() {
+         res.render('index', { 
+            title: 'Recipe.db',
+            recipes: recipes
+         });
+      });
+   });
+  } else {
+   res.end('the database is not ready');
+  }
 });
 
 module.exports = router;
